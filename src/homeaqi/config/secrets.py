465708589dde824f56725fb6fcf40e0559ee3ac2
Diego Fierros <dfierros@alumni.princeton.edu>
2023-07-09 14:47:56 -0400

    Adding template secrets file

---
+++ src/homeaqi/config/secrets.py
@@ -0,0 +1,10 @@
+# Secrets file for AQI script configuration
+
+secrets = {
+    "timezone": "America/New_York",  # http://worldtimeapi.org/timezones
+    "aio_username": None,
+    "aio_key": None,
+    "latitude": None,  # https://www.latlong.net/
+    "longitude": None,
+    "elevation": None,
+}
