344984580b8c5ed91f5c1c6cb1ebf42c3a5b1f35
Diego Fierros <dfierros@alumni.princeton.edu>
2024-01-10 16:59:32 -0500

    Adding some docstrings to aqi_categories

--- src/homeaqi/resources/aqi_categories.py
+++ src/homeaqi/resources/aqi_categories.py
@@ -1,15 +1,29 @@
-# AQI Categories enumerations
+#!/usr/bin/env python
+"""
+AQI categories enumerations
+
+"""
 from __future__ import annotations
 
 from enum import Enum
 
 
 class StrEnum(str, Enum):
+    """
+    Extension of Enum module for use with strings
+
+    """
+
     def __str__(self) -> str:
         return self.value
 
 
 class StrMinMaxEnum(StrEnum):
+    """
+    Extension of StrEnum module for use with strings and float ranges
+
+    """
+
     def __new__(cls, value: str, minimum: float, maximum: float):
         obj = str.__new__(cls, value)
         obj._value_ = value
@@ -19,6 +33,11 @@ class StrMinMaxEnum(StrEnum):
 
 
 class AQISensorBreakpoints(StrMinMaxEnum):
+    """
+    Implementation of StrMinMaxEnum for specifying named float ranges in AQI sensor output
+
+    """
+
     GOOD = "Good", 0.0, 12.0
     MODERATE = "Moderate", 12.1, 35.4
     UNHEALTHY_SENSITIVE = "Unhealthy for Sensitive Groups (USG)", 35.5, 55.4
